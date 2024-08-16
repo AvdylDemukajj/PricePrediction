cb8dbd9ddf9c8f481f1c818d415b492e9d87733d
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-08-16 19:08:24 +0200

    "fixinig a error on gender coloumn"

--- PredictedPrice.py
+++ PredictedPrice.py
@@ -1,18 +1,20 @@
 import pandas as pd
 import numpy as np 
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import LabelEncoder
 
 data = pd.read_csv('car_purchasing_data.csv')
-data
+
+le = LabelEncoder()
+data['gender'] = le.fit_transform(data['gender'])
 
 X = data[['gender', 'age', 'salary', 'debt', 'worth']]
-
 y = data['amount']
 
 model = LinearRegression()
 model.fit(X, y)
 
 new_data = np.array([[1, 33, 75500, 15645, 335845]])
-
 predicted_price = model.predict(new_data)
-print('purchasing price will be: ', predicted_price)
+
+print('Predicted purchasing price will be:', predicted_price[0])
